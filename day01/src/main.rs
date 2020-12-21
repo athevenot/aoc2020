1b59976f3e886dd0470ffbb325cc690695e55d29
Arthur Thévenot <moerius@DESKTOP-1CLDSUP.localdomain>
2020-12-21 14:32:11 +0100

    day one

---
+++ day01/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut input: Vec<u32> = include_str!("../assets/input_puzzle.txt")
+        .lines()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    input.sort();
+    input.reverse();
+    for a in &input {
+        for b in &input {
+            for c in &input {
+                if a + b + c == 2020 {
+                    println!("{}", a * b * c);
+                    return ;
+                }
+            }
+        }
+    }
+}
