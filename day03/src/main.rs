307735cff071e89a1c6e41690d3db61a6329b20d
Arthur Thévenot <moerius@DESKTOP-1CLDSUP.localdomain>
2020-12-22 09:55:34 +0100

    day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+// We use BufReader since we won't go back in file and we don't need to store it in memory.
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("assets/input.txt")?;
+    let mut reader = BufReader::new(f);
+    let mut i: usize = 0;
+    let mut num_tree: usize = 0;
+    loop {
+        let mut buf: [u8; 32] = [0; 32];
+        let len = reader.read(&mut buf[..])?;
+        if is_tree(buf[i]) { num_tree += 1; }
+        if len == 0 { break } // When file over, leave the infinite loop
+        i += 3;
+        if i >= 31 {
+            i %= 31;
+        }
+    }
+    println!("Santa hits {} trees.", num_tree);
+    Ok(())
+}
+
+fn is_tree(c: u8) -> bool {
+    if c as char == '#' {
+        return true
+    }
+    false
 }
